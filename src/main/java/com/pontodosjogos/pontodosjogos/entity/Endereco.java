5e5061bb778c03235956cb95108d83b2c0cbbd0f
Victor Wesley <victor.infowb@gmail.com>
2025-10-21 18:35:39 -0300

    chore: Corrige espaçamento entre linhas

--- src/main/java/com/pontodosjogos/pontodosjogos/entity/Endereco.java
+++ src/main/java/com/pontodosjogos/pontodosjogos/entity/Endereco.java
@@ -11,11 +11,17 @@ public class Endereco {
     private Long id;
 
     private String rua;
+
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
     private String cidade;
+
     private String estado;
+
     private String cep;
 
     @ManyToOne
