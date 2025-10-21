7483e63e1b74082211cb8742dd1ceadd592f0d0d
Victor Wesley <victor.infowb@gmail.com>
2025-10-21 16:25:43 -0300

    feat: Adiciona entidade Endereco

---
+++ src/main/java/com/pontodosjogos/pontodosjogos/entity/Endereco.java
@@ -0,0 +1,22 @@
+package com.pontodosjogos.pontodosjogos.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+
+}
