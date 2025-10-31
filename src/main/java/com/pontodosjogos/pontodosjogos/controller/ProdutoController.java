4269f357e7efc132f6a8ece8616712cf3e193c51
Victor Wesley <victor.infowb@gmail.com>
2025-10-30 22:53:37 -0300

    feat: Adiciona ProdutoController e Estrutura inicial UsuarioController

--- src/main/java/com/pontodosjogos/pontodosjogos/controller/ProdutoController.java
+++ src/main/java/com/pontodosjogos/pontodosjogos/controller/ProdutoController.java
@@ -1,6 +1,30 @@
 package com.pontodosjogos.pontodosjogos.controller;
 
+import com.pontodosjogos.pontodosjogos.controller.request.ProdutoRequest;
+import com.pontodosjogos.pontodosjogos.controller.response.ProdutoResponse;
+import com.pontodosjogos.pontodosjogos.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+@RequiredArgsConstructor
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponse> criarProduto(@RequestBody ProdutoRequest request) {
+        ProdutoResponse response = produtoService.criarProduto(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
     
 
 }
---
+++ src/main/java/com/pontodosjogos/pontodosjogos/controller/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.pontodosjogos.pontodosjogos.controller;
+
+public class UsuarioController {
+}
