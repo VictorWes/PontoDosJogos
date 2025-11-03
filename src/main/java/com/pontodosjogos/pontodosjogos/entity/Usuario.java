1a2e0fc6f8438a361dea6b02bc5e4bb8928cbb2a
Victor Wesley <victor.infowb@gmail.com>
2025-11-03 18:20:06 -0300

    fix: Correção getPassword, onde foi necessario a inserção do return this.senha para que fosse possivel realizar a comparação das senhas  e autenticação do usuario

--- src/main/java/com/pontodosjogos/pontodosjogos/entity/Usuario.java
+++ src/main/java/com/pontodosjogos/pontodosjogos/entity/Usuario.java
@@ -2,7 +2,10 @@ package com.pontodosjogos.pontodosjogos.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Builder
@@ -12,7 +15,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +35,38 @@ public class Usuario {
     private List<Endereco> enderecos;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
